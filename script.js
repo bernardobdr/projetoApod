f29b5fbf655e2d5a4bd86110ff8f6a6dfdacd5ec
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-13 23:23:04 -0300

    alteração no botao de enviar

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ $('.enviar').click(function(){
     let dataFoto = $('#dataFoto').val()
 
     $.ajax({
-        url:`https://api.nasa.gov/planetary/apod?api_key=mQSKc92TwToxbbJWQArJb1hf38etLSarcysdaXwN&date=${dataFoto}
-        `
+        url:`https://api.nasa.gov/planetary/apod?api_key=mQSKc92TwToxbbJWQArJb1hf38etLSarcysdaXwN&date=${dataFoto}`,
+        type: 'GET',
+        success: function(pegarImagem){
+            console.log(pegarImagem)
+        }
     }
     )
 
