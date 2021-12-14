cfaf0b43adb2687691f25ff50db29a184378c6e5
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-14 17:22:21 -0300

    finalizando javascript

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ $('.enviar').click(function(){
         type: 'GET',
         success: function(imagemDoDia){
             console.log(imagemDoDia)
+            $('#titulo').text(imagemDoDia.tltle)
+            $('#descricaoFoto').text(imagemDoDia.explanation)
+            $('#foto').attr('src', imagemDoDia.url)
+            
         }
     })
+})
+
+$.ajax({
+    url:`https://api.nasa.gov/planetary/apod?api_key=If038K0asBSljpY2HtQWwy0fefuPkqSBsxCpeKqT`,
+    type: 'GET',
+    success: function(imagemDoDia){
+        console.log(imagemDoDia)
+        $('#descricaoFoto').text(imagemDoDia.explanation)
+        $('#foto').attr('src', imagemDoDia.url)
+        $('#titulo').text(imagemDoDia.title)
+    }
 })
