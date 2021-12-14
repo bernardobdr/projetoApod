5659064b2b49ece1f37b22cab1e02d2aa7b4e84e
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-14 14:45:31 -0300

    html e css finalizado

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $('.enviar').click(function(){
-    
-    let dataFoto = $('#dataFoto').val() 
 
-    $.jquery({
+    let dataFoto = $('#dataFoto').val()
+    
+    $.ajax({
         url:`https://api.nasa.gov/planetary/apod?api_key=If038K0asBSljpY2HtQWwy0fefuPkqSBsxCpeKqT&date=${dataFoto}`,
         type: 'GET',
-        success: function(pegarFoto){
-            console.log(pegarFoto)
+        success: function(pegarImagem){
+            console.log(pegarImagem)
         }
     })
+})
