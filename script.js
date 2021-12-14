bfbac881606ba787bbcc2e010a71766e879aca95
bernardobdr <bernardosilvabdr@gmail.com>
2021-12-14 14:03:06 -0300

    finalizando html e css do projeto apod

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 $('.enviar').click(function(){
     
     let dataFoto = $('#dataFoto').val()
-
-    $.ajax({
-        url:`https://api.nasa.gov/planetary/apod?api_key=mQSKc92TwToxbbJWQArJb1hf38etLSarcysdaXwN&date=${dataFoto}`,
-        type: 'GET',
-        success: function(pegarImagem){
-            console.log(pegarImagem)
-        }
-    }
-    )
-
-})
